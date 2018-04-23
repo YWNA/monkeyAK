672b03e5fd1242172779282b780d104905b9b224
chenbo <chenbo@edusoho.com>
2018-04-23 10:09:01 +0800

    container

---
+++ src/Provider/LogProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenbo
+ * Date: 18-4-23
+ * Time: 上午9:57
+ */
+
+namespace Monkey\Provider;
+
+
+class LogProvider
+{
+
+}
---
+++ src/Service/Service.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenbo
+ * Date: 18-4-23
+ * Time: 上午9:53
+ */
+
+namespace Monkey\Service;
+
+
+class Service
+{
+
+}
