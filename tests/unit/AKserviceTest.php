b7fa207c0d3521bc5f368943b5563fb2094c51c5
chenbo <chenbo@edusoho.com>
2018-04-26 10:08:26 +0800

    add codeception

---
+++ tests/unit/AKserviceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class AKserviceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $this->tester->assertEquals(1,1);
+        $this->tester->s('users', array('name' => 'Davert', 'email' => 'davert@mail.com'));
+        $this->tester->seeInDatabase('users', array('name' => 'Davert', 'email' => 'davert@mail.com'));
+    }
+}
