145b2639c8313019421202a81f1a732b2e411620
chenbo <chenbo@edusoho.com>
2018-05-03 09:42:54 +0800

    fix table access_secret_key

--- migrations/20180426102305_AccessKeySecretKey.php
+++ migrations/20180426102305_AccessKeySecretKey.php
@@ -10,7 +10,7 @@ class AccessKeySecretKey extends Migration
     public function up()
     {
         $db = $this->container['db'];
-        $db->exec('CREATE TABLE `access_secret_key` ( `id` INT NOT NULL AUTO_INCREMENT , `access_key` VARCHAR(20) NOT NULL , `secret_key` VARCHAR(20) NOT NULL , `created_time` INT(10) NOT NULL , `updated_time` INT(10) NOT NULL , PRIMARY KEY (`id`), UNIQUE `ak` (`access_key`, `secret_key`)) ENGINE = InnoDB;');
+        $db->exec('CREATE TABLE `access_secret_key` ( `id` INT NOT NULL AUTO_INCREMENT , `access_key` VARCHAR(32) NOT NULL , `secret_key` VARCHAR(32) NOT NULL , `created_time` INT(10) NOT NULL , `updated_time` INT(10) NOT NULL , PRIMARY KEY (`id`), UNIQUE `ak` (`access_key`, `secret_key`)) ENGINE = InnoDB;');
     }
 
     /**
