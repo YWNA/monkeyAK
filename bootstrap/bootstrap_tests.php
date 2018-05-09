963a9a1cc67840c8a286b934d23139923fed545b
chenbo <chenbo@edusoho.com>
2018-05-09 14:59:50 +0800

    refactor 单元测试

--- bootstrap/bootstrap_tests.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env php
-<?php
-exec_command('IN_TESTING=true vendor/bin/phpmig migrate');
-function exec_command($command)
-{
-    echo "[exec] {$command}\n";
-    passthru($command);
-}
--- tests/unit/_bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chenbo
- * Date: 18-4-26
- */
-require_once ROOT_DIR . 'bootstrap/bootstrap_tests.php';
